e340133894c2f76b0c14e0779d3aa5a439516b19
Lucas Arnaldo Dessunti <144287816+Ldessunti@users.noreply.github.com>
2024-12-01 12:26:07 -0300

    Delete src directory

--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('dbbarber', 'postgres', '123123', {
-    host: 'localhost',
-    port: 5432,
-    dialect: 'postgres',
-});
-
-module.exports = sequelize;
