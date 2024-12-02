f94da94e9247650b19899a180ad9bfabdacae521
Lucas Arnaldo Dessunti <144287816+Ldessunti@users.noreply.github.com>
2024-12-01 23:07:16 -0300

    update App

--- frontend/App.js
+++ frontend/App.js
@@ -7,6 +7,8 @@ import Home from './src/pages/Home';
 import Produto from './src/pages/Produtos/ProdutoRead';
 import Corte from './src/pages/Cortes/CorteRead';
 import Barbeiro from './src/pages/Barbeiros/BarbeiroRead';
+import Venda from './src/pages/Venda/VendaRead';
+import Cliente from './src/pages/Cliente/ClienteRead';
 import Agendamento from './src/pages/Agendamentos/AgendamentoRead';
 
 import CortesAdd from './src/pages/Cortes/CorteAdd';
@@ -18,7 +20,12 @@ import ProdutosUpdate from './src/pages/Produtos/ProdutoPut';
 import BarbeirosAdd from './src/pages/Barbeiros/BarbeiroAdd';
 import BarbeirosUpdate from './src/pages/Barbeiros/BarbeiroPut';
 
-//import AgendamentosAdd from './src/pages/Agendamentos/AgendamentoAdd';
+import VendasAdd from './src/pages/Venda/VendaAdd';
+import VendasUpdate from './src/pages/Venda/VendaPut';
+
+import ClientesUpdate from './src/pages/Cliente/ClientePut';
+
+import AgendamentosAdd from './src/pages/Agendamentos/AgendamentoAdd';
 //import AgendamentosUpdate from './src/pages/Agendamentos/AgendamentoPut';
 
 
@@ -35,6 +42,8 @@ export default function App() {
         <Stack.Screen name="Produtos" component={Produto} />
         <Stack.Screen name="Cortes" component={Corte} />
         <Stack.Screen name="Barbeiros" component={Barbeiro} />
+        <Stack.Screen name="Vendas" component={Venda} />
+        <Stack.Screen name="Clientes" component={Cliente} />
 
         <Stack.Screen name="CortesAdd" component={CortesAdd} />
         <Stack.Screen name="CortesUpdate" component={CortesUpdate} />
@@ -45,12 +54,17 @@ export default function App() {
         <Stack.Screen name="BarbeirosAdd" component={BarbeirosAdd} />
         <Stack.Screen name="BarbeirosUpdate" component={BarbeirosUpdate} />
 
-      
+        <Stack.Screen name="VendasAdd" component={VendasAdd} />
+        <Stack.Screen name="VendasUpdate" component={VendasUpdate} />
+
+        <Stack.Screen name="ClientesUpdate" component={ClientesUpdate} />
+
+        <Stack.Screen name="AgendamentosAdd" component={AgendamentosAdd} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 //<Stack.Screen name="Agendamentos" component={Agendamento} />
-//<Stack.Screen name="AgendamentosAdd" component={AgendamentosAdd} />
+
 //<Stack.Screen name="AgendamentosUpdate" component={AgendamentosUpdate} />
