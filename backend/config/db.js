fb4a8e76c7e884eae4850f43c20206389f937fba
Lucas Arnaldo Dessunti <144287816+Ldessunti@users.noreply.github.com>
2024-12-01 23:05:54 -0300

    update DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,9 @@ const sequelize = new Sequelize('dbbarber', 'postgres', '123123', {
     host: 'localhost',
     port: 5432,
     dialect: 'postgres',
+    define: {
+        timestamps: false,
+    },
 });
 
 module.exports = sequelize;
